00a47c22a114a277a39e7fb7056082ae6c0339b2
Ivan Paez <ivan.paez@adlinktech.com>
2019-04-24 17:46:42 +0200

    added thr test get in serial and parallel, bug in the parallel option

---
+++ examples/throughput/ythrg.ml
@@ -0,0 +1,75 @@
+open Apero
+open Yaks_ocaml
+open Yaks
+open Yaks.Infix
+open Lwt.Infix
+open Cmdliner
+
+let addr = Arg.(value & opt string "127.0.0.1" & info ["a"; "addr"] ~docv:"ADDRESS" ~doc:"address")
+let port = Arg.(value & opt string "7887" & info ["p"; "port"] ~docv:"PORT" ~doc:"port")
+let samples = Arg.(value & opt int 10000 & info ["n"; "samples"] ~docv:"SAMPLES" ~doc:"number of samples")
+let size = Arg.(value & opt int 1024 & info ["s"; "size"] ~docv:"SIZE" ~doc:"payload size")
+let exec_type = Arg.(value & opt string "s" & info ["t"; "exec_type"] ~docv:"EXEC_TYPE" ~doc:"s:serial or p:parallel")
+
+(* performs gets in parallel*)
+let get_n_p n selector ws  = 
+    let rec get_n_p n selector ws  = 
+        if n > 1 then
+            begin
+                (* let%lwt _ = Lwt_io.printf "get %d\n" n in *)
+                let p = Yaks.Workspace.get selector ws >|= ignore in 
+                let ps = get_n_p (n-1) selector ws in 
+                p :: ps
+            end 
+        else [Yaks.Workspace.get selector ws >|= ignore] in 
+    Lwt.join @@ get_n_p n selector ws
+
+(* performs gets in serial*)
+let rec get_n_s n selector ws  = 
+    if n > 1 then
+        begin
+            let%lwt _ = Yaks.Workspace.get selector ws in 
+            get_n_s (n-1) selector ws
+        end 
+    else Yaks.Workspace.get selector ws >|= ignore
+
+
+let create_data n =
+    let rec r_create_data n s = 
+        if n > 0 then
+            r_create_data (n-1) (s ^ (string_of_int @@ n mod 10))
+        else s
+    in  r_create_data n ""
+
+let run addr port samples size exec_type =
+  Lwt_main.run 
+  (
+    let locator = Apero.Option.get @@ Apero_net.Locator.of_string @@ Printf.sprintf "tcp/%s:%s" addr port in  
+    let%lwt y = Yaks.login locator Properties.empty in 
+    let%lwt ws = Yaks.workspace ~//"/" y  in
+    let value = Value.StringValue (create_data size) in 
+    let _ = Yaks.Workspace.put ~//"test/thr/get" value ws in 
+
+    let selector = ~/*"/test/thr/get" in 
+    let start = Unix.gettimeofday () in 
+    let%lwt () = (match exec_type with
+                    | "p" | "P" -> get_n_p samples selector ws
+                    | _ -> get_n_s samples selector ws) in
+    let stop = Unix.gettimeofday () in 
+    let delta = stop -. start in 
+    let%lwt _ = Lwt_io.printf "Throughput: %f\n" ((float_of_int samples) /. delta) in
+
+    Lwt.return_unit
+  )
+
+
+
+let () =
+    (* let addr = Array.get Sys.argv 1 in
+    let port = Array.get Sys.argv 2 in 
+    let samples = int_of_string (Array.get Sys.argv 3) in
+    let size = int_of_string (Array.get Sys.argv 4) in
+    let locator = Apero.Option.get @@ Apero_net.Locator.of_string @@ Printf.sprintf "tcp/%s:%s" addr port in      
+    Lwt_main.run @@ run locator samples size *)
+    let _ = Term.(eval (const run $ addr $port $ samples $ size $exec_type, Term.info "ythrg")) in  ()
+
